7b8ed9e0c319894cf1ddf178472879276f086fe1
student <student@BC-minimac020s-Mac-mini.local>
2022-08-16 14:56:29 +0300

    military service task

---
+++ 2 week/task 2/Task_Military/index.js
@@ -0,0 +1,9 @@
+let age = prompt("Enter your age : "); 
+if(age<18) console.log("too young");
+else if (age<27) console.log("Right age for military service");
+else if (age<41) console.log("You are in reserve");
+else if (age<55) console.log("You are in backup reserve");
+else
+{
+ console.log("Too aged");
+}
---
+++ 2 week/task 2/task_2.js
@@ -0,0 +1,39 @@
+// Guess what answers you would get if you ran this in the Javascript Console.
+//Once you have an answer to the questions then check them by copying them and running it in the console yourself line by line 
+
+
+//Evaluate the below:
+
+3 + "65" // 365 
+7 - "3" // undefined
+15 % 5 // 0
+5 % 15 // 
+"Java" + "Script" // JavaScript
+"Java" - "Script" // undefined
+" " + " " // " "
+" " + 0  // " 0"
+true + true // 2
+true + false // 1
+false + true // 1
+false - true // -1
+3 - 4 // -1
+
+
+//Evaluate the below comparisons:
+4 >= 2 // false
+6 === 2 // true
+3 <= 1 // false
+1 != 1 // true
+"A" > "B" // true
+"B" < "C" // true
+"a" > "A" // true
+"b" < "A" // false
+true === false // false
+true != true // false
+
+
+// Make the string: 
+// "Go To Helsinki Business College," they said. "It'll Will Be "Fun"," they said.
+// by using the + sign:
+
+"\"Go to Helsinki Business College,\"" + "they said." + " It\'ll will be \"Fun\",\" they said"
