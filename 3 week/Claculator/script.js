1f9c309ae9a0dfc2d4a2bdb95ff38b3d6430ab8c
Ibssa Nebiat <s2200731@BC-C02J64ACQ6LR-MacBook-Air.local>
2022-08-24 12:31:23 +0300

    gasoline calulator

--- 3 week/Claculator/script.js
+++ 3 week/Claculator/script.js
@@ -1,3 +1,7 @@
 const calculateGasoline = (price, money) => {
-    console.log(price,money);
+    const result = money/price;
+    let result_text = "";
+    if (result>10) result_text=`You could get ${Math.floor(result)} litres , good, you can escape now!`;
+    else result_text="Oops, you have to stay here!";
+    document.querySelector('#result').textContent=result_text;
 }
