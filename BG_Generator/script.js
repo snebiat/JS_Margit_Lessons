9ca1a06311c2d797cd0c482511600e1a6c593260
Ibssa Nebiat <s2200731@BCC02J6CQ6LRAir.home>
2022-09-11 00:28:01 +0300

    Add BG generator

--- BG_Generator/script.js
+++ BG_Generator/script.js
@@ -1,9 +1,42 @@
-const gradient_direction = document.querySelector('input[name="direction"]'); 
-const output = document.querySelector('#bg_show'); 
+const gradient_direction = document.querySelectorAll('input[type="radio"]'); 
 
-const generateGradient = () => {
-    console.log('testing');
-/* output.style.background = 'linear-gradient' */
+let gradient_angle;
+
+const output = document.getElementById('bg_show');
+const css_output = document.querySelector('#css_result');
+ 
+
+const generateGradient = (id) => {
+    const first_color = document.querySelector('#first_color').value; 
+    const second_color = document.querySelector('#second_color').value; 
+    switch (id) {
+        case 'id_left_down' : 
+            angle = 'to top left';
+            break; 
+        case 'id_down' : 
+            angle = 'to top';
+            break;
+        case 'id_right_down':
+            angle = 'to top right';
+            break;
+        case 'id_left_middle':
+            angle = 'to left';
+            break;
+        case 'id_right_middle':
+            angle = 'to right';
+            break;
+        case 'id_left_up':
+            angle = 'to bottom left';
+            break;
+        case 'id_up':
+            angle = 'to bottom';
+            break;
+        case 'id_right_up':
+            angle = 'to bottom right';
+            break;
+    } 
+    output.style.background = `linear-gradient(${angle}, ${first_color}, ${second_color})`; 
+    css_output.textContent = `linear-gradient(${angle}, ${first_color}, ${second_color});`; 
 }
 
 
@@ -12,4 +45,6 @@ const generateGradient = () => {
 
 
 
-gradient_direction.addEventListener('click',generateGradient);
+
+
+/* document.querySelectorAll('input[type="radio"]').forEach(item => { item.addEventListener('click',generateGradient(item.id));}); */
