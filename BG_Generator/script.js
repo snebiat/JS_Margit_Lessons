6afc94a9042ed8b39c9d6cbad29017ade813ce69
Ibssa Nebiat <s2200731@BC-C02J64ACQ6LR-MacBook-Air.local>
2022-09-13 12:31:22 +0300

    Add form and remove switch case

--- BG_Generator/script.js
+++ BG_Generator/script.js
@@ -1,50 +1,20 @@
+const form = document.querySelector('#direction_form'); 
 const gradient_direction = document.querySelectorAll('input[type="radio"]'); 
-
-let gradient_angle;
-
 const output = document.getElementById('bg_show');
 const css_output = document.querySelector('#css_result');
- 
+let gradient_angle;
 
 const generateGradient = (id) => {
     const first_color = document.querySelector('#first_color').value; 
     const second_color = document.querySelector('#second_color').value; 
-    switch (id) {
-        case 'id_left_down' : 
-            angle = 'to top left';
-            break; 
-        case 'id_down' : 
-            angle = 'to top';
-            break;
-        case 'id_right_down':
-            angle = 'to top right';
-            break;
-        case 'id_left_middle':
-            angle = 'to left';
-            break;
-        case 'id_right_middle':
-            angle = 'to right';
-            break;
-        case 'id_left_up':
-            angle = 'to bottom left';
-            break;
-        case 'id_up':
-            angle = 'to bottom';
-            break;
-        case 'id_right_up':
-            angle = 'to bottom right';
-            break;
-    } 
-    output.style.background = `linear-gradient(${angle}, ${first_color}, ${second_color})`; 
-    css_output.textContent = `linear-gradient(${angle}, ${first_color}, ${second_color});`; 
+    for (const current of gradient_direction) {
+        if (current.checked) gradient_angle = current.value;
+    }
+    
+    //document.body.style.backgroundImage = `linear-gradient(${gradient_angle}, ${first_color}, ${second_color})`; 
+    output.style.backgroundImage = `linear-gradient(${gradient_angle}, ${first_color}, ${second_color})`; 
+    css_output.textContent = `linear-gradient(${gradient_angle}, ${first_color}, ${second_color});`; 
 }
 
 
-
-
-
-
-
-
-
-/* document.querySelectorAll('input[type="radio"]').forEach(item => { item.addEventListener('click',generateGradient(item.id));}); */
+form.addEventListener('change', generateGradient);
